9ae84f89a488023e1de9d9e1c72419940e7cad8f
Kelvin-Ben <kevoben14@gmail.com>
2024-07-19 20:34:04 +0300

    Create Redux duck pattern

---
+++ data.js
@@ -0,0 +1,13 @@
+const allRecipesData = [
+  { id: 0, name: "Biscuits", img: "./src/assets/images/biscuits.jpg" },
+  { id: 1, name: "Bulgogi", img: "./src/assets/images/bulgogi.png" },
+  { id: 2, name: "Calamari", img: "./src/assets/images/calmari.jpg" },
+  { id: 3, name: "Ceviche", img: "./src/assets/images/Ceviche.jpg" },
+  { id: 4, name: "Cheeseburger", img: "./src/assets/images/cheeseburger.jpg" },
+  { id: 5, name: "Churrasco", img: "./src/assets/images/churrasco.jpg" },
+  { id: 6, name: "Dumplings", img: "./src/assets/images/Dumplings.jpg" },
+  { id: 7, name: "Fish & Chips", img: "./src/assets/images/fishnchips.jpg" },
+  { id: 8, name: "Hummus", img: "./src/assets/images/hummus.jpg" },
+  { id: 9, name: "Masala Dosa", img: "./src/assets/images/masaladosa.jpg" },
+  { id: 10, name: "Pad Thai", img: "./src/assets/images/padthai.jpg" },
+];
